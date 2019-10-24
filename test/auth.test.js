db109902d376744d23a3e41fff3d4d4d20f432b3
Jack McConnell <jackmc1122@gmail.com>
2019-10-24 14:01:41 -0700

    Add basic signup route and tests

---
+++ test/auth.test.js
@@ -0,0 +1,44 @@
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('Bank Ledger auth routes', () => {
+  it('creates a new account on signup', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({ username: 'test-name', password: 'test-password' })
+      .then(res => {
+        expect(res.status).toEqual(200);
+        expect(res.body).toEqual({
+          username: 'test-name',
+          balance: 0
+        });
+      });
+  });
+
+  it('fails to create an account without a username', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({ username: '', password: 'test-password' })
+      .then(res => {
+        expect(res.status).toEqual(400);
+      });
+  });
+
+  it('fails to create an account without a password', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({ username: 'password-test-name', password: '' })
+      .then(res => {
+        expect(res.status).toEqual(400);
+      });
+  });
+
+  it('fails to create an account with a previously used username', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({ username: 'test-name', password: 'other-password' })
+      .then(res => {
+        expect(res.status).toEqual(400);
+      });
+  });
+});
